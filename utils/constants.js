4ec55e4dca2e7dbb56029710f2c129885c4c56f6
vedmitt <ronya14@mail.ru>
2023-08-27 04:27:17 +0800

    fix: video regex fixed

--- utils/constants.js
+++ utils/constants.js
@@ -1,2 +1,2 @@
 module.exports.imageUrlRegex = /(http(s?):)([/|.|\w|\s|-])*\.(?:jpg|jpeg|gif|png|bmp)/;
-module.exports.videoUrlRegex = /(http(s?):)([/|.|\w|\s|-])*\.(?:mp4|mov|wmv|avi|avchd|flv|f4v|swf|mkv|webm)/;
+module.exports.videoUrlRegex = /(http(s?):)([/|.|\w|\s|-])*\./;
